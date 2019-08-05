a2f4f122c6d035cd8335ac8b7c97124f5f022a10
Christian Pena <christianp@tarte.com>
2019-08-04 21:39:05 -0400

    Add toggle support

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -14,19 +14,23 @@ var TodoApp = React.createClass({
             todos: [
                 {
                     id: uuid(),
-                    text: 'Walk the dog'
+                    text: 'Walk the dog',
+                    completed: false
                 },
                 {
                     id: uuid(),
-                    text: 'Clean the yard'
+                    text: 'Clean the yard',
+                    completed: true
                 },
                 {
                     id: uuid(),
-                    text: 'Leave mail on porch'
+                    text: 'Leave mail on porch',
+                    completed: true
                 },
                 {
                     id: uuid(),
-                    text: 'Take out the trash'
+                    text: 'Take out the trash',
+                    completed: false
                 }
             ]
         };
@@ -37,9 +41,22 @@ var TodoApp = React.createClass({
                 ...this.state.todos,
                 {
                     id: uuid(),
-                    text: text
+                    text: text,
+                    completed: false
                 }
             ]
+        });
+    },
+    handleToggle: function (id) {
+        var updatedTodos = this.state.todos.map((todo) => {
+            if(todo.id === id) {
+                todo.completed = !todo.completed;
+            }
+
+            return todo;
+        });
+        this.setState({
+            todos: updatedTodos
         });
     },
     handleSearch: function(showCompleted, searchText) {
@@ -53,7 +70,7 @@ var TodoApp = React.createClass({
         return(
             <div>
                 <TodoSearch onSearch={this.handleSearch} />
-                <TodoList todos={todos} />
+                <TodoList todos={todos} onToggle={this.handleToggle} />
                 <AppTodo onAddTodo={this.handleAddTodo} />
             </div>
         );
